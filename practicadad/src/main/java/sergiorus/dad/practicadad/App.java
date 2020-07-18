a7bd7552b6e8cc1472ae2c023f44fcff41cb8ef4
giruza <sergiorusglez@hotmail.com>
2020-07-18 15:12:43 +0200

    Añadir proyecto maven a git

---
+++ practicadad/src/main/java/sergiorus/dad/practicadad/App.java
@@ -0,0 +1,13 @@
+package sergiorus.dad.practicadad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
