274c545067bddd59ccd98079699f072cc24429aa
giruza <sergiorusglez@hotmail.com>
2020-07-18 21:57:34 +0200

    Licencia apache 2.0
    añadida licencia

--- practicadad/src/main/java/sergiorus/dad/practicadad/ApplicationController.java
+++ practicadad/src/main/java/sergiorus/dad/practicadad/ApplicationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ApplicationController {
 
 	@RequestMapping ("/greeting")
-	public String greetingController(Model model) {
+	public String greetingsController(Model model) {
 		return "greeting_template";
 	}
 }
